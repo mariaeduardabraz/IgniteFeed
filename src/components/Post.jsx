b322019cb9b392e2c0443105e2e8ffd26b6dc2ff
mariaeduardabraz <m2004brazlira@outlook.com>
2023-08-16 14:02:06 -0300

    post finished

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,7 +3,7 @@ import styles from './Post.module.css'
 export function Post () {
     return (
         <article className={styles.post}>
-            <header>
+            <header className={styles.header}>
                 <div className={styles.author}>
                     <img className={styles.avatar} src='https://github.com/mariaeduardabraz.png'/>
                     <div className={styles.authorInfo}>
@@ -17,7 +17,8 @@ export function Post () {
                 <p>Fala galeraa 👋</p>
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. 
                 O nome do projeto é DoctorCare 🚀</p>
-                <p>👉 #irrul</p>
+                <a href='https://github.com/mariaeduardabraz.png'>👉 https://github.com/mariaeduardabraz.png</a>
+                <p>👉 <a href=""> #irrul</a></p>
             </div>
         </article>
     )
