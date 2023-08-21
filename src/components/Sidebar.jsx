5f63a7e07d5004f408efaa5af6e59bffe4f3d769
mariaeduardabraz <m2004brazlira@outlook.com>
2023-08-21 13:41:57 -0300

    avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 import { PencilLine } from 'phosphor-react'
 
@@ -8,7 +9,7 @@ export function Sidebar () {
                 src='https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50'
             />
             <div className={styles.profile}>
-                <img className={styles.avatar} src='https://github.com/mariaeduardabraz.png'/>
+                <Avatar src='https://github.com/mariaeduardabraz.png'/>
                 <strong>Maria Eduarda</strong>
                 <span>Web Dev</span>
             </div>
